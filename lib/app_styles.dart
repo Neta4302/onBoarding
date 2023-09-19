864f539dccdddeb655b517c997df2606834301d2
Neta4302 <54069403+Neta4302@users.noreply.github.com>
2023-09-19 23:52:18 +0700

    Update app_styles.dart

--- lib/app_styles.dart
+++ lib/app_styles.dart
@@ -4,13 +4,16 @@ import 'package:onboarding/size_configs.dart';
 Color primaryColor = const Color(0xffFC9D45);
 Color secondaryColor = const Color(0xff573353);
 
+// Try to adjust the size of font *if you get the oversized fonts*
 final title = TextStyle(
   fontFamily: 'Klasik',
+  // Change the size from 7 to 6, then return 6 to 7 back
   fontSize: SizeConfig.blockSizeH! * 7,
   color: secondaryColor,
 );
 
 final bodyText = TextStyle(
+  // 4.5 to 3, then 3 to 4.5
   fontSize: SizeConfig.blockSizeH! * 4.5,
   color: secondaryColor,
   fontWeight: FontWeight.bold,
