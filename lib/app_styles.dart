f221ea874d537ee77fe327a50f2217433c569713
Neta4302 <54069403+Neta4302@users.noreply.github.com>
2023-09-19 23:53:35 +0700

    Update app_styles.dart

--- lib/app_styles.dart
+++ lib/app_styles.dart
@@ -9,12 +9,14 @@ final title = TextStyle(
   fontFamily: 'Klasik',
   // Change the size from 7 to 6, then return 6 to 7 back
   fontSize: SizeConfig.blockSizeH! * 7,
+  // fontSize: SizeConfig.blockSizeH! * 6,
   color: secondaryColor,
 );
 
 final bodyText = TextStyle(
   // 4.5 to 3, then 3 to 4.5
   fontSize: SizeConfig.blockSizeH! * 4.5,
+  // fontSize: SizeConfig.blockSizeH! * 3,
   color: secondaryColor,
   fontWeight: FontWeight.bold,
 );
